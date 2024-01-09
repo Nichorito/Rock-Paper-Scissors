f6114b093fca9e4a4cef0f8965535eface38e9f2
Nicholas <nsheppard999@gmail.com>
2024-01-08 22:15:05 -0400

    Adding loop function
    Added the function that enables draw looping and a counter to keep
    track of player and computer scores
    
    Next step is to add the win or lose messages when a player hits 5 points

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
-let computerChoice = getComputerChoice();
-let playerChoice = prompt("Please enter 'Rock', 'Paper', or 'Scissors'.");
+game();
 
-playerChoice = playerChoice.toLowerCase();
+function game() {
+    for (let i = 0; i <= 5;) {
+        let computerScore = 0;
+        let playerScore = 0;
+        let computerChoice = getComputerChoice();
+        let playerChoice = prompt("Please enter 'Rock', 'Paper', or 'Scissors'.");
 
-console.log("You chose: " + playerChoice);
-console.log("The COMPUTER chose: " + computerChoice);
+        playerChoice = playerChoice.toLowerCase();
 
-result = start(computerChoice, playerChoice)
+        console.log("You chose: " + playerChoice);
+        console.log("The COMPUTER chose: " + computerChoice);
 
+        result = start(computerChoice, playerChoice);
+        
+        if (result === "draw"){
+            setup();
+        } else if (result === "lose") {
+            computerScore++;
+        } else {
+            playerScore++;
+        }
+
+        console.log("Your score is: " + playerScore + "The COMPUTERS score is: " + computerScore)
+    }
+}
 
 function start(computerChoice, playerChoice) {
-
     
     if (computerChoice === playerChoice) {
-        let result = "Draw";
-        console.log("WOW ITS A DRAW!");
+        let result = "draw";
+        console.log("WOW ITS A DRAW!  Play Again");
         return result;
     } else if ((playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "scissors" && computerChoice === "rock") || (playerChoice === "paper" && computerChoice === "scissors")) {
-        let result = "Lose";
+        let result = "lose";
         console.log("YOU LOSE HAHAHAHAHAHAHAHA WHAT A DUMMY");
         return result;
     } else {
-        let result = "Win";
+        let result = "win";
         console.log("CONGRATULATIONS YOU ARE THE WINNER WINNER CHICKEN DINNER");
         return result;
     }
