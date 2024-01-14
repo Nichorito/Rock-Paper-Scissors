afc3b8fbf2e1c2d9500c533babb18d2b71d7d6e2
Nicholas <nsheppard999@gmail.com>
2024-01-14 14:26:49 -0400

    Switching over to button ui
    Remove the outdated playchoice method, which used console entries for the
    selection.
    
    Replace playerChoice with button inputs for a simplified experience.
    
    Removed the game function as it would not be needed with the onclick
    functions.  Replaced with a score function that runs every time a button
    is clicked

--- script.js
+++ script.js
@@ -1,46 +1,64 @@
 let computerScore = 0;
 let playerScore = 0;
-game();
+let playerChoice = '';
+let result = '';
 
-function game() {   
-    for (let i = 0; i <= 5;) {
-        
-        let computerChoice = getComputerChoice();
-        let playerChoice = prompt("Please enter 'Rock', 'Paper', or 'Scissors'.");
+//Stores html buttons in variables for later onClick events
+let rockBtn = document.querySelector('#rock');
+let paperBtn = document.querySelector('#paper');
+let scissorsBtn = document.querySelector('#scissors');
 
-        playerChoice = playerChoice.toLowerCase();
 
-        console.log("\nYou chose: " + playerChoice);
-        console.log("The COMPUTER chose: " + computerChoice);
+//Runs a function that plays a round when the rock button is clicked
+rockBtn.onclick = () => {
+    playerChoice = 'rock';    
+    result = start(playerChoice);
+    score(result);
+};
 
-        result = start(computerChoice, playerChoice);
-        
-        if (result === "draw"){
-            game();
-        } else if (result === "lose") {
-            computerScore++;
-        } else {
-            playerScore++;
-        }
+//Runs a function that plays a round when the paper button is clicked
+paperBtn.onclick = () => {
+    playerChoice = 'paper';
+    result = start(playerChoice);
+    score(result);
+}
 
-        console.log("\nYour score is: " + playerScore + "\n" + 
-                    "The COMPUTERS score is: " + computerScore);
-
-        if (playerScore === 3) {
-            console.log("CONGRATULATIONS YOU WON THE GAME");
-            break;
-        } else if (computerScore === 3) {
-            console.log("THE COMPUTER WON >:D");
-            break;
-        }
-
-    }
-    console.log("\n\nGAME OVER")
+//Runs a function that plays a round when the scissors button is clicked    
+scissorsBtn.onclick = () => {
+    playerChoice = 'scissors';
+    result = start(playerChoice);
+    score(result);
 }
 
 
-function start(computerChoice, playerChoice) {
+function score(result) {
+
+    if (result === "win"){
+        playerScore++;
+    } 
+    else if (result === "lose") {
+        computerScore++;
+    }
     
+    console.log("\nYour score is: " + playerScore + "\n" + 
+                "The COMPUTERS score is: " + computerScore);
+
+    if (playerScore === 3) {
+        console.log("CONGRATULATIONS YOU WON THE GAME");
+    } else if (computerScore === 3) {
+        console.log("THE COMPUTER WON >:D");
+    }
+}
+
+
+    
+function start(playerChoice) {
+    
+    let computerChoice = getComputerChoice();
+
+    console.log("\nYou chose: " + playerChoice);
+    console.log("The COMPUTER chose: " + computerChoice);
+
     if (computerChoice === playerChoice) {
         let result = "draw";
         console.log("\nWOW ITS A DRAW!  Play Again\n");
